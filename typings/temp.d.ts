bc63655482b0bc93291547102f46d8e25948d96b
Heds Simons <heds@whaleway.net>
2017-02-02 13:51:50 +0000

    Correct typings for the `mkdir` function.

--- typings/temp.d.ts
+++ typings/temp.d.ts
@@ -1,4 +1,4 @@
 declare module 'temp' {
     function track(): void;
-    function mkdir(prefix: string): (err: Error, dirPath: string) => void;
+    function mkdir(prefix: string, cb: (err: Error, dirPath: string) => void): void;
 }
