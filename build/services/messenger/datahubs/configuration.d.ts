7e445a197e80a680417f18e507922d2c0a40efb0
Lucian Buzzo <lucian.buzzo@gmail.com>
2017-10-06 12:57:28 +0100

    Run build step in Docker build process
    To enable multi-stage builds, we've now switched to CircleCI 2.0
    
    Fixes #288
    
    change-type: patch

--- build/services/messenger/datahubs/configuration.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Promise from 'bluebird';
-import { DataHub } from './datahub';
-export declare class ConfigurationDataHub implements DataHub {
-    private prefix;
-    constructor(prefix: string);
-    fetchValue(user: string, service: string, key: string): Promise<string>;
-}
-export declare function createDataHub(prefix: string): DataHub;
